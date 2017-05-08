d09ad1f99d717a4ad1f7acff08875822cbd1ffa4
liubin <18629665928@163.com>
2017-05-08 23:28:14 +0800

    添加tinker修复

--- app/src/main/java/com/example/tinkertest/AActivity.java
+++ app/src/main/java/com/example/tinkertest/AActivity.java
@@ -1,7 +1,12 @@
 package com.example.tinkertest;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
 public class AActivity extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class AActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+        TinkerInstaller.onReceiveUpgradePatch(this, Environment.getExternalStorageDirectory().getAbsolutePath() + "/test");
+        TextView tv = (TextView) findViewById(R.id.tva);
+        tv.setText("我是修复之后的app");
     }
 }
